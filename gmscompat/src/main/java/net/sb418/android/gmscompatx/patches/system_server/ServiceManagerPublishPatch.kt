f7237c9cf74f468cdb72be96af068a8c42e02a23
Lobanokivan11 <59411324+Lobanokivan11@users.noreply.github.com>
2026-07-29 19:15:32 +0300

    Update ServiceManagerPublishPatch.kt

--- gmscompat/src/main/java/net/sb418/android/gmscompatx/patches/system_server/ServiceManagerPublishPatch.kt
+++ gmscompat/src/main/java/net/sb418/android/gmscompatx/patches/system_server/ServiceManagerPublishPatch.kt
@@ -5,6 +5,7 @@
 
 package net.sb418.android.gmscompatx.patches.system_server
 
+import android.content.Context
 import android.os.IBinder
 import android.util.Log
 import de.robv.android.xposed.XC_MethodHook
@@ -19,7 +20,14 @@ object ServiceManagerPublishPatch : IPatch {
             val amsClass = Class.forName("com.android.server.am.ActivityManagerService")
             XposedBridge.hookAllMethods(amsClass, "systemReady", object : XC_MethodHook() {
                 override fun beforeHookedMethod(param: MethodHookParam) {
-                    publishGmsCompatService()
+                    val amsInstance = param.thisObject
+                    val systemContext = XposedHelpers.getObjectField(amsInstance, "mContext") as? Context
+                    
+                    if (systemContext != null) {
+                        publishGmsCompatService(systemContext)
+                    } else {
+                        Log.e(TAG, "GMSCompatX: Unable to extract mContext from ActivityManagerService")
+                    }
                 }
             })
         } catch (t: Throwable) {
@@ -27,16 +35,23 @@ object ServiceManagerPublishPatch : IPatch {
         }
     }
 
-    private fun publishGmsCompatService() {
+    private fun publishGmsCompatService(systemContext: Context) {
         try {
-            val smClass = Class.forName("android.os.ServiceManager")
-            val addServiceMethod = smClass.getMethod("addService", String::class.java, IBinder::class.java)
-            val binderGms2GcaClass = Class.forName("app.grapheneos.gmscompat.BinderGms2Gca")
+            val gmsCompatContext = systemContext.createPackageContext(
+                "app.grapheneos.gmscompat",
+                Context.CONTEXT_INCLUDE_CODE or Context.CONTEXT_IGNORE_SECURITY
+            )
+            val gmsCompatClassLoader = gmsCompatContext.classLoader
+            val binderGms2GcaClass = Class.forName("app.grapheneos.gmscompat.BinderGms2Gca", false, gmsCompatClassLoader)
             val instanceField = binderGms2GcaClass.getDeclaredField("INSTANCE")
             instanceField.isAccessible = true
             val binderInstance = instanceField.get(null) as IBinder
+            val smClass = Class.forName("android.os.ServiceManager")
+            val addServiceMethod = smClass.getMethod("addService", String::class.java, IBinder::class.java)
             addServiceMethod.invoke(null, "com.android.internal.gmscompat.IGms2Gca", binderInstance)
-            Log.d(TAG, "GMSCompatX: Successfully forced published IGms2Gca directly to System ServiceManager!")
+            Log.d(TAG, "GMSCompatX: Successfully forced published IGms2Gca directly to System ServiceManager via Application ClassLoader!")
+        } catch (e: ClassNotFoundException) {
+            Log.w(TAG, "GMSCompatX: app.grapheneos.gmscompat is not installed on this system, skipping publication.")
         } catch (t: Throwable) {
             Log.e(TAG, "GMSCompatX: Failed to dynamically publish IGms2Gca service to system registry", t)
         }
