49ec7aeaf0e8e951ffd2fcf5215ea836713f8b49
Lobanokivan11 <59411324+Lobanokivan11@users.noreply.github.com>
2026-07-29 16:46:29 +0300

    Update BinderProviderPatch.kt

--- gmscompat/src/main/java/net/sb418/android/gmscompatx/patches/BinderProviderPatch.kt
+++ gmscompat/src/main/java/net/sb418/android/gmscompatx/patches/BinderProviderPatch.kt
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-package net.sb418.android.gmscompatx.patches
+package net.sb418.android.gmscompatx.patches.gmscompat
 
 import android.os.Bundle
 import android.os.IBinder
@@ -12,13 +12,12 @@ import de.robv.android.xposed.XC_MethodHook
 import de.robv.android.xposed.XposedHelpers
 import net.sb418.android.gmscompatx.patches.IPatch
 
-object BinderProviderPatch : IPatch {
+object BinderProviderPatch {
     private const val TAG = "GMSCompatX.BinderProviderPatch"
     private const val KEY_BINDER = "binder"
-
-    override fun install() {
+    fun install(classLoader: ClassLoader) {
         try {
-            val providerClass = Class.forName("app.grapheneos.gmscompat.BinderProvider")
+            val providerClass = Class.forName("app.grapheneos.gmscompat.BinderProvider", false, classLoader)
             
             XposedHelpers.findAndHookMethod(
                 providerClass,
@@ -29,17 +28,19 @@ object BinderProviderPatch : IPatch {
                 object : XC_MethodHook() {
                     override fun afterHookedMethod(param: MethodHookParam) {
                         val methodArg = param.args[0] as String
-                        val binderClass = Class.forName("app.grapheneos.gmscompat.BinderGms2Gca")
+                        val binderClass = Class.forName("app.grapheneos.gmscompat.BinderGms2Gca", false, classLoader)
                         val instanceField = binderClass.getDeclaredField("INSTANCE")
                         instanceField.isAccessible = true
                         val rawBinder = instanceField.get(null) as IBinder
                         val replyBundle = Bundle()
                         replyBundle.putBinder(KEY_BINDER, rawBinder)
+                        
                         param.result = replyBundle
                         Log.d(TAG, "Successfully intercepted BinderProvider#call and bypassed Android Sandbox restrictions for type: $methodArg")
                     }
                 }
             )
+            Log.d(TAG, "BinderProviderPatch successfully hooks target class!")
         } catch (t: Throwable) {
             Log.e(TAG, "Failed to install BinderProviderPatch", t)
         }
