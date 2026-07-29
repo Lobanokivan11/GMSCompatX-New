6ab65950dfb5e18598629a064bc7cf0d5ca8d0e4
Lobanokivan11 <59411324+Lobanokivan11@users.noreply.github.com>
2026-07-29 16:46:51 +0300

    Update BinderProviderPatch.kt

--- gmscompat/src/main/java/net/sb418/android/gmscompatx/patches/BinderProviderPatch.kt
+++ gmscompat/src/main/java/net/sb418/android/gmscompatx/patches/BinderProviderPatch.kt
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-package net.sb418.android.gmscompatx.patches.gmscompat
+package net.sb418.android.gmscompatx.patches
 
 import android.os.Bundle
 import android.os.IBinder
