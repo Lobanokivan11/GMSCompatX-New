c6277f12812a3e8dbd9aa149775d7ab80da825d3
Lobanokivan11 <59411324+Lobanokivan11@users.noreply.github.com>
2026-07-29 16:56:50 +0300

    Update BinderProviderPatch.kt

--- gmscompat/src/main/java/net/sb418/android/gmscompatx/patches/BinderProviderPatch.kt
+++ gmscompat/src/main/java/net/sb418/android/gmscompatx/patches/BinderProviderPatch.kt
@@ -9,7 +9,7 @@ import android.os.Bundle
 import android.os.IBinder
 import android.util.Log
 import de.robv.android.xposed.XC_MethodHook
-import de.robv.android.xposed.XposedHelpers
+import de.robv.android.xposed.XposedBridge
 import net.sb418.android.gmscompatx.patches.IPatch
 
 object BinderProviderPatch {
@@ -19,28 +19,20 @@ object BinderProviderPatch {
     fun install(classLoader: ClassLoader) {
         try {
             val providerClass = Class.forName("app.grapheneos.gmscompat.BinderProvider", false, classLoader)
-            XposedHelpers.findAndHookMethod(
-                providerClass,
-                "call",
-                String::class.java,
-                String::class.java,
-                String::class.java,
-                Bundle::class.java,
-                object : XC_MethodHook() {
-                    override fun afterHookedMethod(param: MethodHookParam) {
-                        val methodArg = param.args[1] as? String ?: "0"
-                        val binderClass = Class.forName("app.grapheneos.gmscompat.BinderGms2Gca", false, classLoader)
-                        val instanceField = binderClass.getDeclaredField("INSTANCE")
-                        instanceField.isAccessible = true
-                        val rawBinder = instanceField.get(null) as IBinder
-                        val replyBundle = Bundle()
-                        replyBundle.putBinder(KEY_BINDER, rawBinder)
-                        param.result = replyBundle
-                        Log.d(TAG, "Bypassed Android Sandbox: Intercepted BinderProvider#call for type: $methodArg")
-                    }
+            XposedBridge.hookAllMethods(providerClass, "call", object : XC_MethodHook() {
+                override fun afterHookedMethod(param: MethodHookParam) {
+                    val methodArg = param.args.getOrNull(1) as? String ?: "0"
+                    val binderClass = Class.forName("app.grapheneos.gmscompat.BinderGms2Gca", false, classLoader)
+                    val instanceField = binderClass.getDeclaredField("INSTANCE")
+                    instanceField.isAccessible = true
+                    val rawBinder = instanceField.get(null) as IBinder
+                    val replyBundle = Bundle()
+                    replyBundle.putBinder(KEY_BINDER, rawBinder)
+                    param.result = replyBundle
+                    Log.d(TAG, "Bypassed Android Sandbox: Intercepted BinderProvider#call for type: $methodArg")
                 }
-            )
-            Log.d(TAG, "BinderProviderPatch successfully hooks target class with 4-arg signature!")
+            })
+            Log.d(TAG, "BinderProviderPatch successfully hooks inherited ContentProvider#call method!")
         } catch (t: Throwable) {
             Log.e(TAG, "Failed to install BinderProviderPatch", t)
         }
