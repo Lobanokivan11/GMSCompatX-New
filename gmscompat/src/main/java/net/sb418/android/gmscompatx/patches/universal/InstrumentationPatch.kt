4c284e73baeb0da28165cd02beb576face4e95c4
Lobanokivan11 <59411324+Lobanokivan11@users.noreply.github.com>
2026-07-29 20:22:50 +0300

    Update InstrumentationPatch.kt

--- gmscompat/src/main/java/net/sb418/android/gmscompatx/patches/universal/InstrumentationPatch.kt
+++ gmscompat/src/main/java/net/sb418/android/gmscompatx/patches/universal/InstrumentationPatch.kt
@@ -11,77 +11,31 @@ import android.content.Context
 import android.util.Log
 import de.robv.android.xposed.XC_MethodHook
 import de.robv.android.xposed.XposedBridge
-import de.robv.android.xposed.XposedHelpers
 import net.sb418.android.gmscompatx.patches.IPatch
 import net.sb418.android.gmscompatx.patches.UniversalPatches.TAG
 
+/**
+ * Purpose: enable GmsCompat just before the [android.app.Application] is instantiated.
+ *
+ * Implements:
+ * - [94da5aad6c: Instrumentation.java#L1245](https://github.com/GrapheneOS/platform_frameworks_base/blob/94da5aad6c3184e8afc540bd4cda80fa8a1f2cb6/core/java/android/app/Instrumentation.java#L1245)
+ * - [94da5aad6c: Instrumentation.java#L1264](https://github.com/GrapheneOS/platform_frameworks_base/blob/94da5aad6c3184e8afc540bd4cda80fa8a1f2cb6/core/java/android/app/Instrumentation.java#L1264)
+ */
 internal object InstrumentationPatch : IPatch, XC_MethodHook() {
-
 	override fun install() {
+		// hook all overloads of Instrumentation#newApplication
 		XposedBridge.hookAllMethods(Instrumentation::class.java, "newApplication", this)
 	}
 
 	override fun beforeHookedMethod(param: MethodHookParam) {
+		// NOTE: there are two implementations of newApplication, so we scan the arguments for the Context
 		for (arg in param.args) {
 			if (arg is Context) {
-				val packageName = arg.packageName
-				val classLoader = arg.classLoader ?: return
-				if (packageName == "app.grapheneos.gmscompat") {
-					try {
-						val binderClass = Class.forName("app.grapheneos.gmscompat.BinderGms2Gca", false, classLoader)
-						val stubClass = binderClass.superclass
-						XposedBridge.hookMethod(
-							XposedHelpers.findMethodExact(
-								stubClass, "onTransact",
-								Int::class.java, android.os.Parcel::class.java, android.os.Parcel::class.java, Int::class.java
-							),
-							object : XC_MethodHook() {
-								override fun beforeHookedMethod(binderParam: MethodHookParam) {
-									val code = binderParam.args[0] as Int
-									if (code == 218) {
-										try {
-											Log.w(TAG, "GMSCompatX: Successfully intercepted incoming transaction 218 inside gmscompat process! Preventing AbstractMethodError.")
-											
-											val rawBinder = binderParam.thisObject as android.os.IBinder
-											val data = binderParam.args[1] as android.os.Parcel
-											val reply = binderParam.args[2] as android.os.Parcel
-											data.enforceInterface("com.android.internal.gmscompat.IGms2Gca")
-											val processName = data.readString()
-											val iGca2GmsBinder = data.readStrongBinder()
-											val fileProxyBinder = data.readStrongBinder()
-											val iGca2GmsClass = Class.forName(iGca2GmsBinder.interfaceDescriptor, false, classLoader)
-											val iGca2GmsInstance = XposedHelpers.callStaticMethod(iGca2GmsClass, "asInterface", iGca2GmsBinder)
-											val gmsCompatConfigInstance = XposedHelpers.callMethod(rawBinder, "connectGmsCore", processName, iGca2GmsInstance)
-											reply.writeNoException()
-											if (gmsCompatConfigInstance != null) {
-												reply.writeInt(1)
-												XposedHelpers.callMethod(gmsCompatConfigInstance, "writeToParcel", reply, 0)
-											} else {
-												reply.writeInt(0)
-											}
-											binderParam.result = true
-											return
-										} catch (t: Throwable) {
-											Log.e(TAG, "GMSCompatX: Internal fallback error during transaction 218 processing", t)
-										}
-									}
-								}
-							}
-						)
-						Log.d(TAG, "GMSCompatX: In-process BinderGms2Gca Stub shield installed successfully!")
-					} catch (t: Throwable) {
-						Log.e(TAG, "GMSCompatX: Critical failure while installing in-process Binder shield", t)
-					}
-				}
-
-				try {
-					GmsCompat.maybeEnable(arg)
-				} catch (t: Throwable) {
-					Log.e(TAG, "GmsCompat.maybeEnable execution safely bypassed", t)
-				}
+				GmsCompat.maybeEnable(arg)
 				return
 			}
 		}
+
 		Log.e(TAG, "newApplication() hook: failed to locate Context in argument list!")
 	}
 }
