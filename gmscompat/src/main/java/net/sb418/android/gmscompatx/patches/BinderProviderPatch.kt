cdb02ebcd4b4b9b58eba6ecfdd7bc9501bf63dac
Lobanokivan11 <59411324+Lobanokivan11@users.noreply.github.com>
2026-07-29 20:54:01 +0300

    Update BinderProviderPatch.kt

--- gmscompat/src/main/java/net/sb418/android/gmscompatx/patches/BinderProviderPatch.kt
+++ gmscompat/src/main/java/net/sb418/android/gmscompatx/patches/BinderProviderPatch.kt
@@ -5,10 +5,8 @@
 
 package net.sb418.android.gmscompatx.patches
 
-import android.os.Binder
 import android.os.Bundle
 import android.os.IBinder
-import android.os.Parcel
 import android.util.Log
 import de.robv.android.xposed.XC_MethodHook
 import de.robv.android.xposed.XposedBridge
@@ -19,61 +17,38 @@ object BinderProviderPatch {
     private const val KEY_BINDER = "binder"
 
     fun install(classLoader: ClassLoader) {
-        val binderGms2GcaClass = try {
-            Class.forName("app.grapheneos.gmscompat.BinderGms2Gca", false, classLoader)
-        } catch (e: ClassNotFoundException) {
-            Log.w(TAG, "GMSCompatX: Class 'app.grapheneos.gmscompat.BinderGms2Gca' not found. Skipping primary Binder hook.")
-            null
-        }
-        if (binderGms2GcaClass != null) {
-            try {
-                XposedBridge.hookMethod(
-                    XposedHelpers.findMethodExact(
-                        Binder::class.java, "onTransact",
-                        Int::class.java, Parcel::class.java, Parcel::class.java, Int::class.java
-                    ),
-                    object : XC_MethodHook() {
-                        override fun beforeHookedMethod(binderParam: MethodHookParam) {
-                            val instance = binderParam.thisObject
-                            if (binderGms2GcaClass.isInstance(instance)) {
-                                val code = binderParam.args[0] as Int
-                                if (code == 218) {
-                                    try {
-                                        Log.w(TAG, "GMSCompatX: Successfully intercepted raw transaction 218 at boot! Preventing AbstractMethodError.")
-                                        
-                                        val data = binderParam.args[1] as Parcel
-                                        val reply = binderParam.args[2] as Parcel
-                                        data.enforceInterface("com.android.internal.gmscompat.IGms2Gca")
-                                        val processName = data.readString()
-                                        val iGca2GmsBinder = data.readStrongBinder()
-                                        val fileProxyBinder = data.readStrongBinder()
-                                        
-                                        val iGca2GmsClass = Class.forName(iGca2GmsBinder.interfaceDescriptor, false, classLoader)
-                                        val iGca2GmsInstance = XposedHelpers.callStaticMethod(iGca2GmsClass, "asInterface", iGca2GmsBinder)
-                                        
-                                        if (fileProxyBinder != null) {
-                                            val fileProxyClass = Class.forName("com.android.internal.gmscompat.dynamite.server.IFileProxyService", false, classLoader)
-                                            val fileProxyInstance = XposedHelpers.callStaticMethod(fileProxyClass, "asInterface", fileProxyBinder)
-                                            XposedHelpers.setStaticObjectField(binderGms2GcaClass, "dynamiteFileProxyService", fileProxyInstance)
-                                        }
-                                        XposedHelpers.callMethod(instance, "connectGmsCore", processName, iGca2GmsBinder)
-                                        reply.writeNoException()
-                                        reply.writeInt(0)
-                                        binderParam.result = true
-                                        return
-                                            
-                                    } catch (t: Throwable) {
-                                        Log.e(TAG, "GMSCompatX: Critical failure inside root IPC transaction redirect", t)
-                                    }
-                                }
+        try {
+            val binderGms2GcaClass = Class.forName("app.grapheneos.gmscompat.BinderGms2Gca", false, classLoader)
+            XposedHelpers.findAndHookMethod(
+                binderGms2GcaClass,
+                "connectGmsCore",
+                String::class.java,
+                IBinder::class.java,
+                "com.android.internal.gmscompat.dynamite.server.IFileProxyService",
+                object : XC_MethodHook() {
+                    override fun beforeHookedMethod(param: MethodHookParam) {
+                        try {
+                            val processName = param.args[0] as? String
+                            val callerBinder = param.args[1] as? IBinder
+                            val fileProxyService = param.args[2]
+
+                            Log.w(TAG, "GMSCompatX: Intercepted connectGmsCore for process: $processName. Preventing AbstractMethodError.")
+                            if (fileProxyService != null) {
+                                XposedHelpers.setStaticObjectField(binderGms2GcaClass, "dynamiteFileProxyService", fileProxyService)
                             }
+                            param.result = null 
+                            
+                        } catch (t: Throwable) {
+                            Log.e(TAG, "GMSCompatX: Error inside connectGmsCore hook hook", t)
                         }
                     }
-                )
-                Log.d(TAG, "GMSCompatX: Root Binder IPC protection shield installed successfully!")
-            } catch (t: Throwable) {
-                Log.e(TAG, "Failed to install root BinderProviderPatch shield", t)
-            }
+                }
+            )
+            Log.d(TAG, "GMSCompatX: High-level connectGmsCore patch installed successfully!")
+        } catch (e: ClassNotFoundException) {
+            Log.w(TAG, "GMSCompatX: BinderGms2Gca not found in this ClassLoader. Target package context mismatch.")
+        } catch (t: Throwable) {
+            Log.e(TAG, "Failed to install primary BinderGms2Gca patch", t)
         }
         try {
             val providerClass = Class.forName("app.grapheneos.gmscompat.BinderProvider", false, classLoader)
