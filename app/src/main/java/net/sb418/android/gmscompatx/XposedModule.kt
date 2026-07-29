781bff1eea4d9efc9fa1142eea8e440c76648b3c
Lobanokivan11 <59411324+Lobanokivan11@users.noreply.github.com>
2026-07-29 16:41:35 +0300

    Update XposedModule.kt

--- app/src/main/java/net/sb418/android/gmscompatx/XposedModule.kt
+++ app/src/main/java/net/sb418/android/gmscompatx/XposedModule.kt
@@ -38,8 +38,9 @@ class XposedModule : IXposedHookZygoteInit, IXposedHookLoadPackage {
 		// ignore if we've already patched this process
 		if (!param.isFirstApplication)
 			return
-		// ignore gmscompat to fix error
+		// apply gmscompat patch to fix error
 		if (param.packageName == "app.grapheneos.gmscompat")
+			net.sb418.android.gmscompatx.patches.BinderProviderPatch.install()
 			return
 		// initialize patch context
 		PatchContext.packageName = param.packageName
